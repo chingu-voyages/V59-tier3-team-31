618f39010c1576ef99ff9061aa78f2c561d28cf3
Emmanuel Ungab <john.ungab7@gmail.com>
2026-01-28 19:03:52 +0800

    add: initial setup for landing page

---
+++ nexlink/app/sections/About/index.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div className="flex items-center justify-center border border-white w-full min-h-screen">
+      <p>About</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ nexlink/app/sections/Footer/index.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="w-full h-16 flex items-center justify-center border border-white">
+      <p>Footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
